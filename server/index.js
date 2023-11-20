37e8f350db5d0f727b9313ac9729dbaa3c26a826
irinagos <irynagos47@gmail.com>
2023-11-20 10:28:47 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 const ws = new WebSocket.Server({ server });
 
